edab6eabf7e0f1a1923eabba7ca720151b87491f
Clelia (Astra) Bertelli <clelia@runllama.ai>
2025-09-05 11:19:45 +0200

    wip: migrate cmd backbone

--- migrate_cmd/migrate_cmd.v
+++ migrate_cmd/migrate_cmd.v
@@ -6,11 +6,13 @@ import os
 
 pub const migration_directory_prod = ".vivadb/migrations/prod/"
 pub const migration_directory_dev = ".vivadb/migrations/dev/"
+pub const migration_file_path = "schema.v.sql"
 
 struct MigrationDetails {
+	pub mut:
 	version string
 	name string
-	description string = ""
+	description string
 }
 
 pub fn create_migration_directory(version string, name string, description string, production bool) ! {
@@ -24,4 +26,45 @@ pub fn create_migration_directory(version string, name string, description strin
 	migr_info := MigrationDetails{version, name, description}
 	os.write_file(full_path + "/migration.json", json.encode(migr_info))!
 	println("Successfully created your migration directory. Find information at: ${full_path}/migration.json")
+}
+
+pub fn read_migration_file() !(MigrationDetails, map[string]string) {
+	content := os.read_file(migration_file_path)!
+	lines := os.read_lines(migration_file_path)!
+	mut migr_dets := MigrationDetails{}
+	for line in lines {
+		if line.contains("---") && line.contains("version:") {
+			_, ver := line.split_once("version:") or {continue}
+			clean_ver := ver.trim_space()
+			migr_dets.version = clean_ver
+		} else if line.contains("---") && line.contains("schema_name:") {
+			_, nm := line.split_once("schema_name:") or {continue}
+			clean_nm := nm.trim_space()
+			migr_dets.name = clean_nm
+		} else if line.contains("---") && line.contains("schema_description:") {
+			_, des := line.split_once("schema_description:") or {continue}
+			clean_des := des.trim_space()
+			migr_dets.description = clean_des
+		} else {
+			continue
+		}
+	}
+	if migr_dets.name == "" || migr_dets.version == "" {
+		panic("Unable to find schema_name and/or version fields in your schema.v.sql file: please check that you included them as comments.")
+	}
+	tables := content.split("TABLE ").filter(!it.starts_with("---"))
+	if tables.len == 0 {
+		panic("Unable to find tables within your schema file, please check that the syntax aligns with the rules")
+	}
+	mut name_to_statement := map[string]string{}
+	for table in tables {
+		key, val := table.split_once("(") or {continue}
+		clean_key := key.trim_space()
+		clean_val := "(" + val
+		name_to_statement[clean_key][clean_val]
+	}
+	if name_to_statement.keys().len == 0 {
+		panic("Unable to map the table names with the table definition statements. Please check that the syntax is valid SQL.")
+	}
+	return migr_dets, name_to_statement
 }
--- new_cmd/new_cmd_test.v
+++ new_cmd/new_cmd_test.v
@@ -9,9 +9,11 @@ fn test_create_new_project() {
 	assert os.exists(tmpdir + "/test/.vivadb/.env")
 	assert os.exists(tmpdir + "/test/.vivadb/compose.yaml")
 	assert os.exists(tmpdir + "/test/schema.v.sql")
+	assert os.exists(tmpdir + "/test/README.md")
 	os.chdir(tmpdir)!
 	create_new_project("localhost", 5432, "user", "postgres", "admin", false, "test_1", false)!
 	assert os.exists(tmpdir + "/test_1/.vivadb/.env.local")
 	assert !os.exists(tmpdir + "/test_1/.vivadb/compose.yaml")
+	assert os.exists(tmpdir + "/test_1/README.md")
 	assert os.exists(tmpdir + "/test_1/schema.v.sql")
 }
